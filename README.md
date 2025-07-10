6cafce2a73357a07861a4e03ec39d8cee73030f1
samarcos <samuel.marcos@nokia.com>
2025-07-10 18:33:41 +0100

    Create Devices API project

---
+++ README.md
@@ -0,0 +1,30 @@
+# Device Manager Application
+
+## Overview
+Device Manager Application is a REST API built with Spring Boot to manage device resources. 
+It provides endpoints to perform CRUD operations on devices.
+
+## Features
+• Create a new device. 🚧
+• Fully and/or partially update an existing device. 🚧
+• Fetch a single device. 🚧
+• Fetch all devices. 🚧
+• Fetch devices by brand. 🚧
+• Fetch devices by state. 🚧
+• Delete a single device. 🚧
+
+## Technologies Used
+- Java
+- Spring Boot
+- Maven
+
+## Getting Started
+
+### Prerequisites
+- Java 21 or higher
+- Maven 3.9 or higher
+
+### Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/samuel-marcos_pedo/device-manager.git
---
+++ src/test/java/smarcos/implementation/PostgresIntegrationTest.java
@@ -0,0 +1,27 @@
+package smarcos.implementation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public abstract class PostgresIntegrationTest {
+    @Container
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17.5")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void setDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
+    }
+}
